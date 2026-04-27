2c47e0743874fef4459c0eaa2e8cf44a1a562d60
cmperez2029 <cmperez2029@clc.pshs.edu.ph>
2026-04-27 22:52:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function addToCart(name, price) {
   updateCartDisplay();
 }
 
-function removeFromCart(index) { 
+function removeFromCart(index) {
   cart.splice(index, 1);
   updateCartDisplay();
 }
@@ -14,14 +14,14 @@ function updateCartDisplay() {
   var cartList = document.getElementById("cart-list");
   var totalDisplay = document.getElementById("cart-total");
   var hiddenOrderInput = document.getElementById("cust_order");
+  
   var total = 0;
   var html = "";
-
   for (var i = 0; i < cart.length; i++) {
     total += cart[i].prod_price;
-    html += "<li class='d-flex justify-content-between align-items-center mb-3' style='font-size: 1.1rem; border-bottom: 2px dashed #333; padding-bottom: 8px;'>";
+    html += "<li class='d-flex justify-content-between align-items-center mb-3 cart-item'>";
     html += "<span>" + cart[i].prod_name + " - ₱" + cart[i].prod_price.toLocaleString() + "</span>";
-    html += "<button class='btn btn-sm btn-danger' onclick='removeFromCart(" + i + ")' style='border: 2px solid #000; font-weight:bold;'>&times;</button>";
+    html += "<button class='btn btn-sm btn-danger del-btn' onclick='removeFromCart(" + i + ")'>&times;</button>";
     html += "</li>";
   }
 
