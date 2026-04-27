54eee5cea636354bbf58cfb1511123016a4395fb
cmperez2029 <cmperez2029@clc.pshs.edu.ph>
2026-04-27 11:19:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function updateCartDisplay() {
 
   for (var i = 0; i < cart.length; i++) {
     total += cart[i].prod_price;
-    html += "<li class='d-flex justify-content-between align-items-center mb-2'>";
+    html += "<li class='d-flex justify-content-between align-items-center mb-3' style='font-size: 1.1rem; border-bottom: 2px dashed #333; padding-bottom: 8px;'>";
     html += "<span>" + cart[i].prod_name + " - ₱" + cart[i].prod_price.toLocaleString() + "</span>";
-    html += "<button class='btn btn-sm btn-outline-danger' onclick='removeFromCart(" + i + ")'>&times;</button>";
+    html += "<button class='btn btn-sm btn-danger' onclick='removeFromCart(" + i + ")' style='border: 2px solid #000; font-weight:bold;'>&times;</button>";
     html += "</li>";
   }
 
@@ -32,4 +32,3 @@ function updateCartDisplay() {
     hiddenOrderInput.value = JSON.stringify(cart);
   }
 }
-
